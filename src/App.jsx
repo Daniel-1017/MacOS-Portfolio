2e0dd8cb7eb4408cf1027814b5e910d5ad608f9f
frimudaniel <frimudaniel17@gmail.com>
2025-11-22 23:49:45 +0100

    Implement text window

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Dock, Navbar, Welcome} from '#components';
-import {Finder, Resume, Safari, Terminal} from "#windows";
+import {Finder, Resume, Safari, Terminal, Text} from "#windows";
 import gsap from 'gsap';
 
 import {Draggable} from "gsap/Draggable";
@@ -17,6 +17,7 @@ const App = () => {
             <Safari />
             <Resume />
             <Finder />
+            <Text />
         </main>
     )
 }
