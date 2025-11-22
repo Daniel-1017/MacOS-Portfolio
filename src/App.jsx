3fad2c977d33c9496bc5c4a85b0d5ffd14b42599
frimudaniel <frimudaniel17@gmail.com>
2025-11-22 21:10:34 +0100

    Implement resume window

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Dock, Navbar, Welcome} from '#components';
-import {Safari, Terminal} from "#windows";
+import {Resume, Safari, Terminal} from "#windows";
 import gsap from 'gsap';
 
 import {Draggable} from "gsap/Draggable";
@@ -15,6 +15,7 @@ const App = () => {
 
             <Terminal />
             <Safari />
+            <Resume />
         </main>
     )
 }
